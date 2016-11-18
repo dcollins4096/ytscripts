1700dfb91c30336bbb4bbabc596431fa0bf0dcbe
dcollins4096 <none@none>
2016-11-18 10:27:22 -0600

    metals and stuff

--- p33_sims.py
+++ p33_sims.py
@@ -3,6 +3,7 @@ bd = '/mnt/c/scratch/sciteam/dcollins/Paper33_Galaxy'
 weight_fields = {'scaled_div_b':'cell_volume','magnetic_field_strength':'density'}
 methods = {'abs_divb':'mip'}
 sim_base_dir = {}
+centers = {}
 sim_base_dir['e02']= 'e02_64_stock_nograckle'
 sim_base_dir['e04']= 'e04_64_mhdct'  
 sim_base_dir['h01']= 'h01_64_stock_newCrosby'
@@ -31,3 +32,44 @@ sim_base_dir['i03'] = 'i03_monkey_around_ppm'
 sim_base_dir['i04'] = 'i04_monkey_around_newPF_ppm'
 sim_base_dir['i05'] = 'i05_i02_nofield'
 sim_base_dir['a01'] = 'a01_agora1'
+sim_base_dir['ai01'] = 'ai01_newDevin'
+sim_base_dir['ai02'] = 'ai02_weak_field'
+sim_base_dir['ai03'] = 'ai03_repeat_i02'
+sim_base_dir['ai14'] = 'ai14_fixed_zerofield'
+sim_base_dir['ai16'] = 'ai16_fixed_tinyfield'
+sim_base_dir['ai17'] = 'ai17_repeat_i02'
+sim_base_dir['ai17b'] = 'ai17b_rerun'
+
+sim_base_dir['ai16b'] = 'ai16b'
+sim_base_dir['ai21b'] = 'ai21b'
+sim_base_dir['ai21b'] = 'ai21b'
+sim_base_dir['ai22b'] = 'ai22b'
+sim_base_dir['ai23b'] = 'ai23b'
+sim_base_dir['ai24b'] = 'ai24b_longerdt'
+centers['ai17'] = nar([ 0.49853515625, 0.49853515625, 0.49853515625])
+centers['ai17b'] = nar([ 0.49853515625, 0.49853515625, 0.49853515625])
+sim_base_dir['ai21'] = 'ai21_ai17_mass'
+sim_base_dir['ai22'] = 'ai22_ai17_32cube'
+sim_base_dir['ai23'] = 'ai23_length_units'
+sim_base_dir['ai24'] = 'ai24_length_64'
+sim_base_dir['ai25'] = 'ai25_smallroot_refine_on_start'
+all_sim_list = ['ai16b','ai17b','ai21b','ai22b','ai23b','ai24b','i02','ai25']
+all_fields_simple = [ "Cooling_Time", "Metal_Density", "GasEnergy", "Temperature", "TotalEnergy"]
+all_fields_1 = ["Cooling_Time",          
+"Electron_Density",      
+"GasEnergy",             
+"H2II_Density",          
+"H2I_Density",           
+"HII_Density",           
+"HI_Density",            
+"HM_Density",            
+"HeIII_Density",         
+"HeII_Density",          
+"HeI_Density",           
+"Metal_Density",         
+"Temperature",           
+"TotalEnergy"]
+other_fields = ["Density",               
+"x-velocity",            
+"y-velocity",            
+"z-velocity"]            
