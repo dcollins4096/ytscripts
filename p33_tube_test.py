afe7e23813247dabc68def8855a2128fb75465fb
dcollins4096 <none@none>
2017-02-14 13:55:17 -0500

    more p33 tube

--- p33_tube_test.py
+++ p33_tube_test.py
@@ -3,9 +3,13 @@ if 'ef' not in dir():
 #ef('tube.py')
 import tube
 reload(tube)
-basedir = '/scratch1/dcollins/Paper33_galaxies/'
+basedir = '/Users/dcollins/scratch/Paper33_galaxies'
 sims = {}
 sims['aj13e'] =  '%s/aj13e_square'%basedir
+sims['aj13g'] =  '%s/aj13g_square_pico'%basedir
+sims['aj13h'] =  '%s/aj13h_square_hancock'%basedir
+sims['aj13i'] =  '%s/aj13i_square_supersonic'%basedir
+sim = 'aj13i'
 axis=0
 coord=(0.505,0.505)
 counter=0
@@ -16,14 +20,12 @@ counter=0
 #fields +=['x-velocity','y-velocity','z-velocity']
 #fields += ['Ltension_x', 
 #fields += ['Badvection_x']
-fields = ['density','Metal_Density']
+fields = ['density','Metal_Density', 'metallicity']
 ray_set={}
-sim = 'aj13e'
-frames=range(0,30,5)+[27]
+frames=[0,3,10] #range(10) #[0,1,5,10] #,2,5] #range(5) #[0,10] #range(30)
 renorm={'density':1,'pressure':0.6,'TotalEnergy':0.9}
 
 ds_list = [yt.load("%s/DD%04d/data%04d"%(sims[sim], frame,frame)) for frame in frames]
-
 y=tube.tube(ds_list,   delta=False, fields=fields,filename = "Tube_%s%s.png"%(sim,"_%s"*len(fields)%tuple(fields)),labels=[""]*100)
 #, labels=fields, plot_args={'marker':'*'})
 
