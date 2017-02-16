8d5cd0ce1b466c605dd706ef94d13edb54579b92
dcollins <none@none>
2017-02-16 13:57:30 -0600

    quan works better, does gravity

--- turb_quan.py
+++ turb_quan.py
@@ -7,6 +7,7 @@ class quan_box():
         self.name = car.name
         self.keys=['ex','ey','ez','vx','vy','vz','px','py','pz','mach']
         self.magkeys=['Bx','By','Bz','bx','by','bz','bx2','by2','bz2','beta','AlfMach','AlfvenSpeed']
+        self.stuff={}
     def __getitem__(self,key):
         return self.__dict__[key]
     def show(self,format="%10.2e"):
@@ -23,112 +24,16 @@ class quan_box():
     def __call__(self,car, tdyn=1, frames=None, extant_quan=None):
         #car.plot()
         print car.name
-        if hasattr(extant_quan,'vx'):
-            self.vx=extant_quan.vx
-        else:
-            self.vx=[]
-        if hasattr(extant_quan,'vy'):
-            self.vy=extant_quan.vy
-        else:
-            self.vy=[]
-        if hasattr(extant_quan,'vz'):
-            self.vz=extant_quan.vz
-        else:
-            self.vz=[]
-        if hasattr(extant_quan,'mach'):
-            self.mach=extant_quan.mach
-        else:
-            self.mach=[]
-        if hasattr(extant_quan,'px'):
-            self.px=extant_quan.px
-        else:
-            self.px=[]
-        if hasattr(extant_quan,'py'):
-            self.py=extant_quan.py
-        else:
-            self.py=[]
-        if hasattr(extant_quan,'pz'):
-            self.pz=extant_quan.pz
-        else:
-            self.pz=[]
-        if hasattr(extant_quan,'ex'):
-            self.ex=extant_quan.ex
-        else:
-            self.ex=[]
-        if hasattr(extant_quan,'ey'):
-            self.ey=extant_quan.ey
-        else:
-            self.ey=[]
-        if hasattr(extant_quan,'ez'):
-            self.ez=extant_quan.ez
-        else:
-            self.ez=[]
-        if hasattr(extant_quan,'t'):
-            self.t=extant_quan.t 
-        else:
-            self.t=[]
 
-        if hasattr(extant_quan,'bx'):
-            self.bx=extant_quan.bx
-        else:
-            self.bx=[]
-        if hasattr(extant_quan,'by'):
-            self.by=extant_quan.by
-        else:
-            self.by=[]
-        if hasattr(extant_quan,'bz'):
-            self.bz=extant_quan.bz
-        else:
-            self.bz=[]
+        all_fields = ['vx','vy','vz','mach','px','py','pz','ex','ey','ez','t','bx','by','bz','bx2','by2','bz2']
+        all_fields +=['Bx','By','Bz','Bfield_strength','AlfMach','beta','AlfvenSpeed','frames']
+        all_fields +=['ke_tot','ke_rel','grav_pot','grav_pot_2','gas_work']
+        for field in all_fields:
+            if extant_quan is not None and extant_quan.stuff.has_key(field):
+                self.stuff[field] = extant_quan[field]
+            else:
+                self.stuff[field] = []
 
-        if hasattr(extant_quan,'bx2'):
-            self.bx2=extant_quan.bx2
-        else:
-            self.bx2=[]
-        if hasattr(extant_quan,'by2'):
-            self.by2=extant_quan.by2
-        else:
-            self.by2=[]
-        if hasattr(extant_quan,'bz2'):
-            self.bz2=extant_quan.bz2
-        else:
-            self.bz2=[]
-
-
-        if hasattr(extant_quan,'Bx'):
-            self.Bx=extant_quan.Bx
-        else:
-            self.Bx=[]
-        if hasattr(extant_quan,'By'):
-            self.By=extant_quan.By
-        else:
-            self.By=[]
-        if hasattr(extant_quan,'Bz'):
-            self.Bz=extant_quan.Bz
-        else:
-            self.Bz=[]
-
-        if hasattr(extant_quan,'Bfield_strength'):
-            self.Bfield_strength=extant_quan.Bfield_strength
-        else:
-            self.Bfield_strength=[]
-
-        if hasattr(extant_quan,'AlfMach'):
-            self.AlfMach=extant_quan.AlfMach 
-        else:
-            self.AlfMach=[]
-        if hasattr(extant_quan,'beta'):
-            self.beta=extant_quan.beta 
-        else:
-            self.beta=[]
-        if hasattr(extant_quan,'AlfvenSpeed'):
-            self.AlfvenSpeed=extant_quan.AlfvenSpeed 
-        else:
-            self.AlfvenSpeed=[]
-        if hasattr(extant_quan,'frames'):
-            self.frames=extant_quan.frames
-        else:
-            self.frames=[]
         if hasattr(extant_quan,'tdyn'):
             self.tdyn = extant_quan.tdyn
         else:
@@ -138,73 +43,81 @@ class quan_box():
             frames = car.frames
 
         car.fill(frames[0])
+        potential_written = False
+        if car.ds['SelfGravity']:
+            car.ds.create_field_info()
+            potential_written = 'PotentialField' in [k[1] for k in car.ds.field_info.keys()]
+
         for frame in frames:
-            if frame in self.frames:
+            if frame in self.stuff['frames']:
                 continue
             print "QUAN ON ", car.name, frame
-            self.frames.append(frame)
+            self.stuff['frames'].append(frame)
             car.fill(frame)
             ds=car.ds
-            self.t.append(car.ds.current_time/(tdyn) )
+            self.stuff['t'].append(car.ds['InitialTime']/(tdyn) )
             car.region_type='all'
             reg = car.get_region(frame)
             total_volume = reg['cell_volume'].sum()
             volume = reg['cell_volume']
-            self.mach.append( np.sqrt(reg.quantities['WeightedAverageQuantity']('mean_square_velocity','cell_volume').v ) )
-            del reg['mean_square_velocity']
-            self.vx.append(reg.quantities['WeightedAverageQuantity']('velocity_x','cell_volume').v)
-            self.vy.append(reg.quantities['WeightedAverageQuantity']('velocity_y','cell_volume').v)
-            self.vz.append(reg.quantities['WeightedAverageQuantity']('velocity_z','cell_volume').v)
-            self.px.append(reg.quantities['WeightedAverageQuantity']('momentum_x','cell_volume').v)
-            self.py.append(reg.quantities['WeightedAverageQuantity']('momentum_y','cell_volume').v)
-            self.pz.append(reg.quantities['WeightedAverageQuantity']('momentum_z','cell_volume').v)
-            for field in ['momentum_x','momentum_y','momentum_z']:
-                del reg[field]
-            self.ex.append(reg.quantities['WeightedAverageQuantity']('eng_x','cell_volume').v)
-            self.ey.append(reg.quantities['WeightedAverageQuantity']('eng_y','cell_volume').v)
-            self.ez.append(reg.quantities['WeightedAverageQuantity']('eng_z','cell_volume').v)
+            self.stuff['mach'].append( np.sqrt(reg.quantities['WeightedAverageQuantity']('mean_square_velocity','cell_volume').in_units('code_velocity**2').v ) )
+            self.stuff['vx'].append(reg.quantities['WeightedAverageQuantity']('velocity_x','cell_volume').in_units('code_velocity').v)
+            self.stuff['vy'].append(reg.quantities['WeightedAverageQuantity']('velocity_y','cell_volume').in_units('code_velocity').v)
+            self.stuff['vz'].append(reg.quantities['WeightedAverageQuantity']('velocity_z','cell_volume').in_units('code_velocity').v)
+            self.stuff['px'].append(reg.quantities['WeightedAverageQuantity']('momentum_x','cell_volume').in_units('code_density*code_velocity').v)
+            self.stuff['py'].append(reg.quantities['WeightedAverageQuantity']('momentum_y','cell_volume').in_units('code_density*code_velocity').v)
+            self.stuff['pz'].append(reg.quantities['WeightedAverageQuantity']('momentum_z','cell_volume').in_units('code_density*code_velocity').v)
+            self.stuff['ex'].append(reg.quantities['WeightedAverageQuantity']('eng_x','cell_volume').in_units('code_density*code_velocity**2').v)
+            self.stuff['ey'].append(reg.quantities['WeightedAverageQuantity']('eng_y','cell_volume').in_units('code_density*code_velocity**2').v)
+            self.stuff['ez'].append(reg.quantities['WeightedAverageQuantity']('eng_z','cell_volume').in_units('code_density*code_velocity**2').v)
+            self.stuff['ke_tot'].append(reg.quantities['WeightedAverageQuantity']('kinetic_energy','cell_volume').in_units('code_density*code_velocity**2').v)
+            reg.set_field_parameter('bulk_velocity',ds.arr([self.stuff['vx'][-1],self.stuff['vy'][-1], self.stuff['vz'][-1]],'code_velocity'))
+            self.stuff['ke_rel'].append(reg.quantities['WeightedAverageQuantity']('rel_kinetic_energy','cell_volume').in_units('code_density*code_velocity**2').v)
+            if potential_written:
+                self.stuff['grav_pot'].append(reg.quantities['WeightedAverageQuantity']('grav_pot','cell_volume').in_units('code_density*code_velocity**2').v)
+            self.stuff['gas_work'].append(reg.quantities['WeightedAverageQuantity']('gas_work','cell_volume').in_units('code_density*code_velocity**2').v)
 
             if car.ds['HydroMethod'] in [4,6]:
-                self.Bx.append( (reg['Bx']*volume).sum()/total_volume)
-                self.By.append( (reg['By']*volume).sum()/total_volume)
-                self.Bz.append( (reg['Bz']*volume).sum()/total_volume)
+                self.stuff['Bx'].append( (reg['Bx']*volume).sum()/total_volume)
+                self.stuff['By'].append( (reg['By']*volume).sum()/total_volume)
+                self.stuff['Bz'].append( (reg['Bz']*volume).sum()/total_volume)
 
-                self.bx.append( ( (reg['Bx']-self.Bx[-1])*volume).sum()/total_volume)
-                self.by.append( ( (reg['By']-self.By[-1])*volume).sum()/total_volume)
-                self.bz.append( ( (reg['Bz']-self.Bz[-1])*volume).sum()/total_volume)
+                self.stuff['bx'].append( ( (reg['Bx']-self.stuff['Bx'][-1])*volume).sum()/total_volume)
+                self.stuff['by'].append( ( (reg['By']-self.stuff['By'][-1])*volume).sum()/total_volume)
+                self.stuff['bz'].append( ( (reg['Bz']-self.stuff['Bz'][-1])*volume).sum()/total_volume)
 
-                self.bx2.append( np.sqrt(( (reg['Bx']-self.Bx[-1])**2*volume).sum()/total_volume) )
-                self.by2.append( np.sqrt(( (reg['By']-self.By[-1])**2*volume).sum()/total_volume) )
-                self.bz2.append( np.sqrt(( (reg['Bz']-self.Bz[-1])**2*volume).sum()/total_volume) )
+                self.stuff['bx2'].append( np.sqrt(( (reg['Bx']-self.stuff['Bx'][-1])**2*volume).sum()/total_volume) )
+                self.stuff['by2'].append( np.sqrt(( (reg['By']-self.stuff['By'][-1])**2*volume).sum()/total_volume) )
+                self.stuff['bz2'].append( np.sqrt(( (reg['Bz']-self.stuff['Bz'][-1])**2*volume).sum()/total_volume) )
 
-                self.Bfield_strength.append( (reg['magnetic_field_strength']*volume).sum()/total_volume)
-                self.AlfvenSpeed.append( (volume*reg['magnetic_field_strength']/np.sqrt(np.pi*4*reg['density']) ).sum()/total_volume)
+                self.stuff['Bfield_strength'].append( (reg['magnetic_field_strength']*volume).sum()/total_volume)
+                self.stuff['AlfvenSpeed'].append( (volume*reg['magnetic_field_strength']/np.sqrt(np.pi*4*reg['density']) ).sum()/total_volume)
 
-                self.AlfMach.append( self.mach[-1]/self.AlfvenSpeed[-1])
-                self.beta.append( 2*(self.AlfMach[-1]/self.mach[-1])**2 )
+                self.stuff['AlfMach'].append( self.stuff['mach'][-1]/self.stuff['AlfvenSpeed'][-1])
+                self.stuff['beta'].append( 2*(self.stuff['AlfMach'][-1]/self.stuff['mach'][-1])**2 )
 
 
 
         if car.ds['HydroMethod'] in [4,6]:
             plt.clf()
-            plt.plot(self.t,self.Bx,label='Bx')
-            plt.plot(self.t,self.By,label='By')
-            plt.plot(self.t,self.Bz,label='Bz')
-            plt.plot(self.t,self.bx,label='bx')
-            plt.plot(self.t,self.by,label='by')
-            plt.plot(self.t,self.bz,label='bz')
-            plt.plot(self.t,self.bx2,label='bx rms')
-            plt.plot(self.t,self.by2,label='by rms')
-            plt.plot(self.t,self.bz2,label='bz rms')
+            plt.plot(self.stuff['t'],self.stuff['Bx'],label='Bx')
+            plt.plot(self.stuff['t'],self.stuff['By'],label='By')
+            plt.plot(self.stuff['t'],self.stuff['Bz'],label='Bz')
+            plt.plot(self.stuff['t'],self.stuff['bx'],label='bx')
+            plt.plot(self.stuff['t'],self.stuff['by'],label='by')
+            plt.plot(self.stuff['t'],self.stuff['bz'],label='bz')
+            plt.plot(self.stuff['t'],self.stuff['bx2'],label='bx rms')
+            plt.plot(self.stuff['t'],self.stuff['by2'],label='by rms')
+            plt.plot(self.stuff['t'],self.stuff['bz2'],label='bz rms')
             plt.legend(loc=0)
             plt.xlabel('t/tcode')
             plt.savefig('p42_Quan_%s_field_strength.pdf'%car.name)
 
             plt.clf()
-            plt.plot(self.t, self.AlfMach, label='MA')
-            plt.plot(self.t, self.mach, label="M")
-            plt.plot(self.t, self.AlfvenSpeed,label='Va')
-            plt.plot(self.t, self.beta, label='beta')
+            plt.plot(self.stuff['t'], self.stuff['AlfMach'], label='MA')
+            plt.plot(self.stuff['t'], self.stuff['mach'], label="M")
+            plt.plot(self.stuff['t'], self.stuff['AlfvenSpeed'],label='Va')
+            plt.plot(self.stuff['t'], self.stuff['beta'], label='beta')
             plt.xlabel('t/tcode')
             plt.legend(loc=0)
             plt.savefig('p42_Quan_%s_MaM.pdf'%car.name)
@@ -213,23 +126,32 @@ class quan_box():
 
 
         plt.clf()
-        plt.plot(self.t,self.ex,label='ex')
-        plt.plot(self.t,self.ey,label='ey')
-        plt.plot(self.t,self.ez,label='ez')
+        plt.plot(self.stuff['t'],self.stuff['ex'],label='ex')
+        plt.plot(self.stuff['t'],self.stuff['ey'],label='ey')
+        plt.plot(self.stuff['t'],self.stuff['ez'],label='ez')
         plt.legend(loc=0)
         plt.savefig('p42_Quan_%s_eng.pdf'%car.name)
 
         plt.clf()
-        plt.plot(self.t,self.px,label='px')
-        plt.plot(self.t,self.py,label='py')
-        plt.plot(self.t,self.pz,label='pz')
+        plt.plot(self.stuff['t'],self.stuff['px'],label='px')
+        plt.plot(self.stuff['t'],self.stuff['py'],label='py')
+        plt.plot(self.stuff['t'],self.stuff['pz'],label='pz')
         plt.legend(loc=0)
         plt.savefig('p42_Quan_%s_mom.pdf'%car.name)
 
         plt.clf()
-        plt.plot(self.t,self.vx,label='vx')
-        plt.plot(self.t,self.vy,label='vy')
-        plt.plot(self.t,self.vz,label='vz')
-        plt.plot(self.t,self.mach,label='mach')
+        plt.plot(self.stuff['t'],self.stuff['vx'],label='vx')
+        plt.plot(self.stuff['t'],self.stuff['vy'],label='vy')
+        plt.plot(self.stuff['t'],self.stuff['vz'],label='vz')
+        plt.plot(self.stuff['t'],self.stuff['mach'],label='mach')
         plt.legend(loc=0)
         plt.savefig('p42_Quan_%s_vel.pdf'%car.name)
+
+        plt.clf()
+        plt.plot(self.stuff['t'],self.stuff['ke_tot'],label='ke_tot')
+        plt.plot(self.stuff['t'],self.stuff['ke_rel'],label='ke_rel')
+        if potential_written: 
+            plt.plot(self.stuff['t'],self.stuff['grav_pot'],label='grav_pot')
+        plt.plot(self.stuff['t'],self.stuff['gas_work'],label='gas_work')
+        plt.legend(loc=0)
+        plt.savefig('p42_Quan_%s_energy.pdf'%car.name)
