6a58652612e293874b9b4c0cae1b139de146abab
dcollins4096 <none@none>
2017-10-19 10:02:41 -0400

    stat in the right place

--- taxi.py
+++ taxi.py
@@ -176,38 +176,6 @@ class fleet():
         print profname
         plt.savefig(profname)
 
-    def stat(self,field,frame=None):
-        """print min and max of *field*"""
-        ds = self.load(frame)
-        minTuple = ds.find_min(field)
-        maxTuple = ds.find_max(field)
-        return {'min':minTuple,'max':maxTuple}
-
-    def mstat(self,fields=None,frames=None, format = "%9.2e",Norm=False):
-        """Min and max for all fields in self.fields.
-        Field list overridden by *fields*.
-        *Norm* subtracts off the volume-weighted mean."""
-        print fields
-        for frame in frames:
-            if fields is None:
-                fields = self.fields
-            self.load(frame)
-            out = self.region.quantities['Extrema'](fields)
-            if Norm is True:
-                for n, field in enumerate(fields):
-                    avg = self.region.quantities['WeightedAverageQuantity'](field,'CellVolume')
-                    out[n] = out[n][0]-avg, out[n][1]-avg
-            if hasattr(Norm,'has_key'):
-                for n, field in enumerate(fields):
-                    avg =  0
-                    if Norm.has_key(field):
-                        avg = Norm[field]
-                        print field,avg
-                    out[n] = out[n][0]-avg, out[n][1]-avg
-            format_string = "%s %s %s"%(format,format,"%s")
-            for n, field in enumerate(fields):
-                print format_string%(out[n][0], out[n][1], field)
-
     def find_extrema(self,fields=None,frames=None, manual_positive=False):
         all_fields = []
         for car in self: #I feel like there's an easier way to do this.
@@ -1431,6 +1399,38 @@ class taxi:
                     raise PortError("Callback %s not supported"%callback)
             else:
                 print "Where the heck did you get that callback at?"
+
+    def stat(self,field,frame=None):
+        """print min and max of *field*"""
+        ds = self.load(frame)
+        minTuple = ds.find_min(field)
+        maxTuple = ds.find_max(field)
+        return {'min':minTuple,'max':maxTuple}
+
+    def mstat(self,fields=None,frames=None, format = "%9.2e",Norm=False):
+        """Min and max for all fields in self.fields.
+        Field list overridden by *fields*.
+        *Norm* subtracts off the volume-weighted mean."""
+        print fields
+        for frame in frames:
+            if fields is None:
+                fields = self.fields
+            self.load(frame)
+            out = self.region.quantities['Extrema'](fields)
+            if Norm is True:
+                for n, field in enumerate(fields):
+                    avg = self.region.quantities['WeightedAverageQuantity'](field,'CellVolume')
+                    out[n] = out[n][0]-avg, out[n][1]-avg
+            if hasattr(Norm,'has_key'):
+                for n, field in enumerate(fields):
+                    avg =  0
+                    if Norm.has_key(field):
+                        avg = Norm[field]
+                        print field,avg
+                    out[n] = out[n][0]-avg, out[n][1]-avg
+            format_string = "%s %s %s"%(format,format,"%s")
+            for n, field in enumerate(fields):
+                print format_string%(out[n][0], out[n][1], field)
 
 class other_horsecrap():
 ######################### stuff not ported
