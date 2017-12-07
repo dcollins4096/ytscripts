52f7b240cdf416c13bf25ba68fe0d4083845f883
dcollins4096 <none@none>
2017-12-07 07:54:34 -0800

    improvements

--- p49_health.py
+++ p49_health.py
@@ -11,17 +11,33 @@ if 'flt' not in dir():
     flt['frames']='last'
     flt('car.get_frames()')
 
+def doit(flt,fun):
+    for car in flt:
+        fun(car)
 
-def update(car):
-    new_pickle_name = 'xfer/quan_box_%s.pickle'%car.name
-    if glob.glob(new_pickle_name) != []:
-        car.qb.stuff.update(fPickle.load(new_pickle_name))
-    else:
-        print "no new pickle found"
+def check_output(car):
+    last_frame=max(car.frame_dict.keys())
+    last_set = car.frame_dict[last_frame]['dsname']
+    last_set_full = "%s/%s"%(car.directory,last_set)
+    print last_set_full
+    print car.name, glob.glob(last_set_full)
 
-class dumb():
-    def __init__(self):
-        pass
+doit(flt, check_output)
+
+def check_quan(car, print_ok=True, print_not_ok = False):
+   file_list = glob.glob("%s/DD????"%car.directory)
+   for f in file_list:
+       frame = int(f[-4:])
+       if frame in car.qb.stuff['frames'] and frame in car.qb.stuff['EB']:
+           if print_ok:
+               a,b=f.split('/')[-2:]
+               print "%s/%s.tar"%(a,b)
+       else:
+           if print_not_ok: 
+               print "NOT OK"
+
+
+
 def print_status(car):
     last_frame=max(car.frame_dict.keys())
     lfd = car.frame_dict[last_frame]
@@ -35,13 +51,20 @@ def print_status(car):
     print "{c.name}, {lfd[SetNumber]}, {lfd[cycle]}, {lfd[time]}, {:d}, {:d}".format( last_quan, last_EB, c=car, lfd=lfd)
     return lfd
 #lfd=print_status(flt[0])
-for car in flt:
-    print_status(car)
-    update(car)
-    print_status(car)
-
-if 'yes_save' in dir():
+def update(car):
+    new_pickle_name = 'xfer/quan_box_%s.pickle'%car.name
+    if glob.glob(new_pickle_name) != []:
+        car.qb.stuff.update(fPickle.load(new_pickle_name))
+    else:
+        print "no new pickle found"
+if 'do_update' in dir():
     for car in flt:
-        car.qb.dump()
-        print "dumped", car.name
+        print_status(car)
+        update(car)
+        print_status(car)
+
+    if 'yes_save' in dir():
+        for car in flt:
+            car.qb.dump()
+            print "dumped", car.name
 
