6e86dd4ddf18eb5349b314f0bb8d79b7f4757c67
dcollins4096 <none@none>
2017-12-08 09:57:37 -0600

    update p49 health

--- p49_health.py
+++ p49_health.py
@@ -4,9 +4,13 @@
 #ac23_Ma6_Ma01_256
 #ac25_M01_Ma01_512
 #ac26_M01_Ma5_512
-
+ef('p49_stuff.py')
 if 'flt' not in dir():
-    flt = taxi.fleet(['ac19','ac22','ac23','ac25','ac26'])
+    #fleet_list = ['ac19','ac22','ac23','ac25','ac26']
+    #fleet_list = sorted(all_sims)
+    #fleet_list = ['ax19','ax20', 'ax21','ax22']
+    fleet_list = sorted(series['az'])
+    flt = taxi.fleet(fleet_list)
     flt('car.qb_load()')
     flt['frames']='last'
     flt('car.get_frames()')
@@ -16,15 +20,16 @@ def doit(flt,fun):
         fun(car)
 
 def check_output(car):
+    """make sure the restart is there"""
     last_frame=max(car.frame_dict.keys())
     last_set = car.frame_dict[last_frame]['dsname']
     last_set_full = "%s/%s"%(car.directory,last_set)
     print last_set_full
     print car.name, glob.glob(last_set_full)
 
-doit(flt, check_output)
 
 def check_quan(car, print_ok=True, print_not_ok = False):
+   """Look at _all_ output for presense in quan and EB"""
    file_list = glob.glob("%s/DD????"%car.directory)
    for f in file_list:
        frame = int(f[-4:])
@@ -36,9 +41,8 @@ def check_quan(car, print_ok=True, print_not_ok = False):
            if print_not_ok: 
                print "NOT OK"
 
-
-
 def print_status(car):
+    """Check last frame, quan, and EB for consistency"""
     last_frame=max(car.frame_dict.keys())
     lfd = car.frame_dict[last_frame]
     last_quan = max(car.qb.stuff['frames'])
@@ -47,9 +51,10 @@ def print_status(car):
     else:
         last_EB = -1
     #{'SetNumber': 242, 'dsname': './DD0242/data0242', 'DirPrefix': './DD', 'time': 1.209999951012777, 'name_files': 'data', 'cycle': 69519}
-
     print "{c.name}, {lfd[SetNumber]}, {lfd[cycle]}, {lfd[time]}, {:d}, {:d}".format( last_quan, last_EB, c=car, lfd=lfd)
-    return lfd
+    #return lfd
+
+doit(flt, print_status)
 #lfd=print_status(flt[0])
 def update(car):
     new_pickle_name = 'xfer/quan_box_%s.pickle'%car.name
