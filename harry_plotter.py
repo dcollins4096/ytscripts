53fe80b5d0578cff15a88eb50400cf2d7e4af8f4
dcollins4096 <none@none>
2018-05-08 08:18:01 -0700

    plot tweak

--- harry_plotter.py
+++ harry_plotter.py
@@ -12,5 +12,12 @@ if len(sys.argv) > 2:
     first = {'ax19':90,'ax20':120,'ax21':300,'ax22':60}[car.name]
     car.frames = s[slice(first,last,5)].tolist()+[last]
 print "XXXXXXX", car.return_frames()
+actual_frames = []
+for frame in car.return_frames():
+    data_name = "%s/%s"%(car.directory,car.frame_dict[frame]['dsname'])
+    if glob.glob(data_name) != []:
+        actual_frames.append(frame)
+
+car.frames=actual_frames
 car.plot()
 
