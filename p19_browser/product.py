d701a9c0adf2df2545a20892e5268fd7369e821b
dcollins4096 <dcollins4096@gmail.com>
2020-07-29 10:52:27 -0400

    intermediate check in

--- p19_browser/product.py
+++ p19_browser/product.py
@@ -8,12 +8,20 @@ class plot():
         self.parameters=parameters
 
 class product():
-    def __init__(self, name="P", regexp="reg",myglob="glob", parameters=['core_id','frame']):
+    def __init__(self, name="P", regexp="reg",myglob="glob", parameters=['core_id','frame'],style='single'):
         self.myglob=myglob
         self.regexp=regexp
         self.name=name
         self.parameters=parameters
         self.plots=defaultdict(lambda: list())
+        self.style=style
+        if style=='single':
+            self.render = self.single_render
+        elif style == 'frames':
+            self.render = self.frame_render
+        else:
+            self.render = None
+        print("Built", self.style, self.render)
 
     def check_glob(self):
         print("check glob")
@@ -40,4 +48,23 @@ class product():
                     sort_key = p
                     break
             self.plots[core_id] = sorted( self.plots[core_id],key= lambda plot : plot.parameters[sort_key])
+
+    def frame_render(self,core_id):
+        out1 = "<td>frame render (%s)</td>"%self.style
+        return out1
+    def single_render(self,core_id):
+        #out1 = "<td>single render (%s)</td>"%self.style
+        self.width=200
+        #img_tag = img_tag_template%(self.plots[core_id].fname)
+        if len( self.plots[core_id]) == 0:
+            img = "x"
+        else:
+            img_tag_template = '<a h<figure><a href="%s"><img src="%s" width=%s></a><figcaption>%s</figcaption></figure>'
+            fname = self.plots[core_id][0].fname
+            img = img_tag_template%(fname, fname, self.width,"")
+
+        out1 = "<td>%s</td>"%img
+
+        return out1 
+
 
