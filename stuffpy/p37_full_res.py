64f57cba634a1fa7c9e980c498a02c49c0a1b60e
dcollins4096 <dcollins4096@gmail.com>
2020-09-15 10:41:25 -0400

    remove stuffpy

--- stuffpy/p37_full_res.py
+++
@@ -1,6 +0,0 @@
-
-plt.clf()
-fig = plt.figure(figsize=[81.92]*2, dpi=100)
-fig.figimage(np.log10(den))
-outname = 'full_res_%s_%s_t%04d_%s'%(simname, subset, frame, resolution_name)
-fig.savefig(outname)
--- stuffpy/p42_old.py
+++
@@ -1,3 +0,0 @@
-
-fname = '/scratch1/dcollins/Paper42_new_turb/aq11_mhd1.0/cycle0000'
-dso = yt.load(fname)
