bdcbd864e57966edc08fb61442ed49c90db56f17
dcollins <dccollins@fsu.edu>
2020-11-13 09:25:08 -0500

    removing extra stuff

--- stuffpy/maptest.py
+++
@@ -1,6 +0,0 @@
-#proj = yt.ProjectionPlot(ds,0,'density')
-for cm in  ['BrBG']: #['Accent','BrBG','Oranges','Purples']:
-  proj.set_cmap('density',cm)
-  #proj.set_zlim('density',0.1,1000)
-  proj.set_zlim('density',0.1,100)
-  proj.save('fyap_%s'%cm)
--- stuffpy/p14_vd2.py
+++
@@ -1,7 +0,0 @@
-
-
-data = at.leaf_clumps[3][0].data
-AvgVelocity = data.ds.arr([(data['cell_mass']*data[v]).sum()/data['cell_mass'].sum()
-                              for v in ['x-velocity','y-velocity','z-velocity']])
-data.set_field_parameter("bulk_velocity",AvgVelocity)
-print data['VelocityDispersionSquared']
